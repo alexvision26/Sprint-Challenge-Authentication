573c062d991bb3335a454474879742b86bfe450d
Alex Miller <alexmillerprojects@gmail.com>
2020-03-27 08:13:03 -0700

    working on auth

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
